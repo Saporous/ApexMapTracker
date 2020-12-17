aa92f740a219722c7546b7b838f98e95a54bf4bd
Jeff-Work <chen.jeffrey.r@gmail.com>
2020-12-16 20:07:05 -0800

    initial commit

---
+++ ApexMapTracker.py
@@ -0,0 +1,15 @@
+# Import stream link
+# TODO: import from list of Apex streamers?
+link = "https://www.twitch.tv/funfps"
+
+# Grab screenshot from stream
+# TODO: Install Streamlink and run as subprocess
+
+# Grab minimap image from screenshot
+# TODO:
+
+# Determine player and map coordinates
+# TODO:
+
+# Output result 
+# TODO:
