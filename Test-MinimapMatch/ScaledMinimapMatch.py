df8aa5eae03ea801f2cf3eeccff384d5cfb25332
Jeff-Home <chen.jeffrey.r@gmail.com>
2020-12-18 03:03:52 -0800

    one more test and print to console best resize ratio and the coordinates of one corner

--- Test-MinimapMatch/ScaledMinimapMatch.py
+++ Test-MinimapMatch/ScaledMinimapMatch.py
@@ -17,20 +17,22 @@ import cv2
 # args = vars(ap.parse_args())
 # load the image image, convert it to grayscale, and detect edges
 # template = cv2.imread(args["template"])
-template = cv2.imread('oly2.png')
+# template = cv2.imread('oly2.png')
+template = cv2.imread('WE3.png')
 template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
 cv2.imshow("Template", template)
 # loop over the images to find the template in
 # for imagePath in glob.glob(args["images"] + "/*.jpg"):
-# for imagePath in glob.glob('WorldsEdge.png'):
-for imagePath in glob.glob('Olympus.png'):
+for imagePath in glob.glob('WorldsEdge.png'):
+# for imagePath in glob.glob('Olympus.png'):
 	# load the image, convert it to grayscale, and initialize the
 	# bookkeeping variable to keep track of the matched region
 	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	found = None
+	scaleVal = None
 	# loop over the scales of the image
 	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
 		# resize the image according to the scale, and keep track
@@ -58,11 +60,15 @@ for imagePath in glob.glob('Olympus.png'):
 		# the bookkeeping variable
 		if found is None or maxVal > found[0]:
 			found = (maxVal, maxLoc, r)
+			scaleVal = scale
 	# unpack the bookkeeping variable and compute the (x, y) coordinates
 	# of the bounding box based on the resized ratio
 	(_, maxLoc, r) = found
 	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
 	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+	print(scaleVal)
+	print(startX)
+	print(startY)
 	# draw a bounding box around the detected result and display the image
 	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 	cv2.imshow("Image", image)
